eec1cc76e1b7002c60ad3892817e4f17ebf41112
Bukharova Victoria <bvv005tkun@gmail.com>
2025-05-01 15:40:55 +0700

    fix(routing): update base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/deckbuilding-roguelike-front/",
   plugins: [react(), tsconfigPaths()],
 });
